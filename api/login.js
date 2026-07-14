2d1699d779e612efc28bae255c5e6b140390f322
DEV || Justin <justinzudolski2011@gmail.com>
2026-07-14 19:13:30 +0200

    Implement login handler with password validation

--- api/login.js
+++ api/login.js
@@ -1,1 +1,19 @@
+export default function handler(req, res) {
 
+    const { password } = req.body;
+
+
+    if(password === process.env.BIO_PASSWORD){
+
+        return res.status(200).json({
+            success:true
+        });
+
+    }
+
+
+    return res.status(401).json({
+        success:false
+    });
+
+}
